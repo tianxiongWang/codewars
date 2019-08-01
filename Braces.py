736cbe67549606f75652642546aad69fab603a2a
tianxiongWang <13961252501@139.com>
2019-08-02 00:35:25 +0800

    为括号判断优化了算法,效率提高了一个数量级,令人震惊!

--- Braces.py
+++ Braces.py
@@ -1,9 +1,9 @@
 # C++不熟练，失败了，先用python实现
 import queue
-
+import time
 string = '[({})](]'
 
-# 这个代码的思路就是创建一个lifo模型，然后查到返向括号就提取，用来判断是否
+# 这个代码的思路就是创建一个lifo模型，然后查到返向括号就提取，用来判断是否正确闭合
 def validBraces(string):
     dic = {'(': ')', '[': ']', '{': '}'}
     List = queue.LifoQueue()
@@ -17,5 +17,37 @@ def validBraces(string):
                 continue
     return True
 
+# 使用另一套思路，用x， z， d代表三个括号，每一次循环这三个数都必须为正数，小于0就说明未闭合，来做一下
+def validBraces2(string):
+    x, z, d = 0, 0, 0
+    for i in string:
+        if i == '(':
+            x += 1
+        if i == '[':
+            z += 1
+        if i == '{':
+            d += 1
+        if i == ')':
+            x -= 1
+        if i == ']':
+            z -= 1
+        if i == '}':
+            d -= 1
+        if x < 0 or z < 0 or d < 0:
+            return False
+            break
+    if x == 0 and z == 0 and d == 0:
+        return True
+    else:
+        return False
+
 if __name__ == "__main__":
+    begin = time.time() * 10 ** 5
     print(validBraces(string))
+    over = time.time() * 10 ** 5
+    print(over - begin)
+    begin = time.time() * 10 ** 5
+    print(validBraces2(string))
+    over = time.time() * 10 ** 5
+    print(over - begin)
+    #方法二效率得到显著提高
